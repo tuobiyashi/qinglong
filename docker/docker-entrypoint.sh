c0be045ad59e2e4971d77ba5201bc5256f1da2d5
tuobiyashi <helong@usa.com>
2021-06-25 20:47:26 +0800

    insert jdc
    添加花语JDC

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -18,6 +18,13 @@ echo -e "======================3. 启动nginx========================\n"
 nginx -s reload 2>/dev/null || nginx -c /etc/nginx/nginx.conf
 echo -e "nginx启动成功...\n"
 
+echo -e "====================== 启动JDC========================\n"
+cd /ql/jdc
+nohup ./JDC &
+cd -
+sleep 3
+echo -e "====================== 启动JDC=END=======================\n"
+
 echo -e "======================4. 启动控制面板========================\n"
 if [[ $(pm2 info panel 2>/dev/null) ]]; then
   pm2 reload panel
